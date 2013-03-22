63daf749d1e3bd59c514ce28f39688af849d0c33
erichens <ecrichens@gmail.com>
2013-03-21 20:57:18 -0400

    pointing to my fork

--- responsive-retrofit-bookmarklet.js
+++ responsive-retrofit-bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function () {  
   var jsCode = document.createElement('script');
-  jsCode.setAttribute('src', 'http://weisjohn.github.com/Responsive-Retrofitting/responsive-retrofit.js');
+  jsCode.setAttribute('src', 'http://emilyweis.github.com/Responsive-Retrofitting/responsive-retrofit.js');
   document.body.appendChild(jsCode);  
 }());
